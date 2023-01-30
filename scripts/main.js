7826f67dd7d1d28b697eaaf799024ceeab0ca2b9
sofasofaaoa <ladyrobertus3@gmail.com>
2023-01-30 10:51:43 +0700

    простите я забыла что надо каждый пункт пушить(((

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,28 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
     }
 })
