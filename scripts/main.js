8a7329a9d51f3553f9b579b289d204df7100aca9
sofasofaaoa <ladyrobertus3@gmail.com>
2023-01-30 09:42:31 +0700

    30.01

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+    }
+})
