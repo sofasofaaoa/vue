4036dac3db481a7004a1f529b29c0858e41af77b
sofasofaaoa <ladyrobertus3@gmail.com>
2023-01-30 11:06:36 +0700

    п.6

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,20 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 100,
+        inventory: 7,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -26,7 +29,18 @@ let app = new Vue({
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            if (this.cart < this.inventory) {
+                this.cart += 1
+            }
+        },
+        removeFromCart() {
+            if (this.cart > 0){
+                this.cart -= 1
+            }
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
         }
+
     }
 })
